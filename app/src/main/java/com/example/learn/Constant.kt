d51a0dc39c5c5963d1854cb4a34c9ae477697d47
Hazemorn <nikitmiheenko@gmail.com>
2023-01-02 22:00:46 +0300

    object Constant

---
+++ app/src/main/java/com/example/learn/Constant.kt
@@ -0,0 +1,15 @@
+package com.example.learn
+
+object Constant {
+
+        const val Director_Salary = 2000
+        const val Manager_Salary =2100
+        const val Sup_Salary =1700
+
+        const val Director = "miha"
+        const val Manager = "linda"
+        const val Sup = "lara"
+
+
+
+}
--- app/src/main/java/com/example/learn/MainActivity.kt
+++ app/src/main/java/com/example/learn/MainActivity.kt
@@ -59,16 +59,6 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
-    object Constant{
-        const val Director_Salary = 2000
-        const val Manager_Salary =2100
-        const val Sup_Salary =1700
-
-        const val Director = "miha"
-        const val Manager = "linda"
-        const val Sup = "lara"
-
-    }
 
 
 }
